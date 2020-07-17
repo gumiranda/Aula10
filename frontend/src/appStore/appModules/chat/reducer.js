75685e9a3eb812c6faa8c29ee3304a9d07e9963c
gumiranda <imartytk@hotmail.com>
2020-07-17 18:08:22 -0300

    paginação de mensagens

--- frontend/src/appStore/appModules/chat/reducer.js
+++ frontend/src/appStore/appModules/chat/reducer.js
@@ -5,6 +5,11 @@ const INITIAL_STATE = {
   chatsList: [],
   chatsPage: 1,
   chatsTotal: 0,
+  messagesLoading: false,
+  messagesList: [],
+  messagesPage: 1,
+  messagesTotal: 0,
+  messagesId: 0,
 };
 
 export default function chat(state = INITIAL_STATE, action) {
@@ -33,10 +38,42 @@ export default function chat(state = INITIAL_STATE, action) {
         draft.chatsTotal = 0;
         break;
       }
+      case '@chat/RESET_DETAILS': {
+        draft.messagesLoading = false;
+        draft.messagesList = [];
+        draft.messagesPage = 1;
+        draft.messagesId = 0;
+        draft.messagesTotal = 0;
+        break;
+      }
+      case '@chat/DETAILS_SUCCESS': {
+        draft.messagesLoading = false;
+        draft.messagesTotal = action.payload.messagesTotal;
+        draft.messagesList = action.payload.messagesList;
+        break;
+      }
+      case '@chat/DETAILS_REQUEST': {
+        draft.messagesLoading = true;
+        draft.messagesTotal = 0;
+        draft.messagesPage = action.payload.page;
+        draft.messagesId = action.payload.id;
+        break;
+      }
+
+      case '@chat/DETAILS_FAILURE': {
+        draft.messagesLoading = false;
+        break;
+      }
       case '@auth/SIGN_OUT': {
+        draft.chatsLoading = false;
         draft.chatsList = [];
         draft.chatsPage = 1;
         draft.chatsTotal = 0;
+        draft.messagesLoading = false;
+        draft.messagesList = [];
+        draft.messagesPage = 1;
+        draft.messagesId = 0;
+        draft.messagesTotal = 0;
         break;
       }
       default:
--- frontend/src/appStore/appModules/chat/sagas.js
+++ frontend/src/appStore/appModules/chat/sagas.js
@@ -2,6 +2,8 @@ import {all, takeLatest, call, put, select} from 'redux-saga/effects';
 import {Alert} from 'react-native';
 import api from '../../../services/api';
 import {getSuccess, getFailure} from './list';
+import {getSuccessDetails, getFailureDetails} from './details';
+import {GiftedChat} from 'react-native-gifted-chat';
 
 export function* getChats({payload}) {
   try {
@@ -28,5 +30,43 @@ export function* getChats({payload}) {
     yield put(getFailure());
   }
 }
-
-export default all([takeLatest('@chat/LIST_REQUEST', getChats)]);
+export function* getChatDetails({payload}) {
+  try {
+    const {nextPage, page, id} = payload;
+    const response = yield call(api.get, `chat/${id}/page/${page}`);
+    if (response.data) {
+      const {countMessages, messages} = response.data;
+      if (!nextPage) {
+        yield put(
+          getSuccessDetails({
+            messagesList: messages.reverse(),
+            messagesTotal: countMessages,
+          }),
+        );
+      } else {
+        const {messagesList} = yield select(state => state.chat);
+        const joinArray = [...messagesList, ...messages.reverse()];
+        const mapa = new Map();
+        for (const x of joinArray) {
+          mapa.set(x._id, x);
+        }
+        const final = [...mapa.values()];
+        yield put(
+          getSuccessDetails({
+            messagesList: final,
+            messagesTotal: countMessages,
+          }),
+        );
+      }
+    } else {
+      yield put(getFailureDetails());
+    }
+  } catch (err) {
+    Alert.alert('Erro', 'Confira seus dados');
+    yield put(getFailureDetails());
+  }
+}
+export default all([
+  takeLatest('@chat/LIST_REQUEST', getChats),
+  takeLatest('@chat/DETAILS_REQUEST', getChatDetails),
+]);
